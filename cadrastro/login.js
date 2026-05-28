53bfba7f402bbe7bd52bb09aaf1ac1bc2a4a0e06
cardosomateus190210-bit <cardosomateus190210@gmail.com>
2026-05-28 13:32:14 -0300

    adiciona pasta cadrastro e todos arquivos

--- cadrastro/login.js
+++ cadrastro/login.js
@@ -31,7 +31,7 @@ items.forEach(item => {
 
 function voltarInicio(){
 
-  window.location.href = "../index.html";
+  window.location.href = "../index.htm";
 
 }
 
@@ -51,7 +51,7 @@ function voltarlogin(){
 
 function entrar1(){
 
-  window.location.href = "../dashboard.html";
+  window.location.href = "../dashboard.htm";
 }
 
 function entrar(){
